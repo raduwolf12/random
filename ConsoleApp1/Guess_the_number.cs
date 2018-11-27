69869032ed746a014d7e2bed0ee2b6e4bac5ae0d
Taraburca Radu <rtaraburca@yahoo.com>
2018-11-27 17:33:55 +0200

    <codul>

---
+++ ConsoleApp1/Guess_the_number.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Guess_the_number
+    { 
+
+       static bool validare(int a)
+    {
+            if (a < 0)
+                return false;
+            if (a > 100)
+                return false;
+            return true;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            Random rng = new Random();
+            int r = rng.Next(0, 100);
+            bool ok = true, ok2=true;
+            Console.WriteLine("Numarul este intre 0 si 100.");
+            Console.WriteLine("Ghiceste numarul!");
+            string str = Console.ReadLine();
+            int numar = int.Parse(str);
+         
+            if (numar == r)
+            {
+                Console.WriteLine("CORECT!");
+                
+                   }
+
+            while (ok==true)
+            {
+                if (validare(numar) == false)
+                {
+                    Console.WriteLine("Numarul ales nu este valid");
+                    ok2 = false;
+                }
+                else
+                    ok2 = true;
+                if (numar!=r)
+                {
+                    Console.WriteLine("Mai incearca odata!");
+                    if(r>numar)
+                        Console.WriteLine("Numarul introdus e mai mic decat numarul care trebuie ghicit!");
+                    else if(ok2==true)
+                        Console.WriteLine("Numarul introdus e mai mare decat numarul care trebuie ghicit!");
+                    Console.WriteLine();
+
+                    str = Console.ReadLine();
+                     numar = int.Parse(str);
+
+                }
+                else
+                {
+                    Console.WriteLine("CORECT!");
+                    ok=false;
+
+                }
+
+            }
+
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            conversie char->int 
+            Console.WriteLine("scrie un nr: ");
+            string str=Console.ReadLine();
+            //int nr=int.Parse(str);
+            int nr = Convert.ToInt32(str);
+            
+            Console.WriteLine("numarul e  {0}",nr+1);
+             */
+          /* 
+           // generare de numere random
+           Random rng = new Random();
+           int r = rng.Next(1, 102);
+           Console.WriteLine("numarul e  {0}",r);
+      */
+
+
+            /* //instructiunea switch;
+             Console.WriteLine("scrie un nr: ");
+             string str = Console.ReadLine();
+             int nr = int.Parse(str);
+             switch(nr)
+             {
+                 case 1:
+                     Console.WriteLine("nr e 1 ");
+                     break;
+                 case 2:
+                     Console.WriteLine("nr e 2 ");
+                     break;
+
+                default:
+                     Console.WriteLine("nu e un nr valid");
+                     break;
+
+
+             }
+             
+    */
+
+
+
+        
+        }
+    }
+}
